88c6aac68eb9856b7e2c58ab44633f30bfad5f7b
FunnyClown <technoevolutioner@gmail.com>
2019-05-30 03:18:04 -0400

    Edit File
    File Yang Di Edit :
    1. Penambahan File htaccess
    2. Pembuatan Controller Default
    3. Pembuatan Halaman Index

---
+++ application/views/beranda/index.php
@@ -0,0 +1,34 @@
+<div class="container mt-5">
+	<div class="row">
+      <div class="col-md-6 sm-12" data-aos="fade-right">
+        <img src="<?= base_url('assets/images/image-1.webp'); ?>" width="550" class="img-fluid">
+      </div>
+
+      <div class="col-md-6" id="desc">
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A nobis laudantium quaerat natus non eum, voluptatem magnam unde, consectetur vel. Recusandae, delectus, praesentium. Impedit architecto, tempore aperiam, est eveniet maiores qui doloremque, placeat labore vitae quae molestias aspernatur doloribus. Quia mollitia dolorum incidunt natus repellat. Ratione et distinctio, modi voluptates.</p>
+              <center>
+              	<a class="btn btn-primary" href="#" role="button">Read More</a>
+          	  </center>
+          </div>
+
+          <div class="carousel-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero sapiente saepe numquam ipsam assumenda! Et vero obcaecati animi sint architecto inventore molestiae doloribus atque eos natus reiciendis minima dignissimos voluptas, aspernatur omnis quisquam cum facere asperiores, delectus culpa ullam blanditiis molestias? Sequi laboriosam neque nam. Eaque fugiat ipsa deserunt aspernatur.</p>
+               <center>
+              	<a class="btn btn-primary" href="#" role="button">Read More</a>
+          	  </center>
+          </div>
+
+          <div class="carousel-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sed, quibusdam error aut corporis dolorem facilis laudantium at ut temporibus dicta voluptatum quia fugiat eligendi perspiciatis cumque velit sequi deserunt impedit ducimus pariatur accusamus. Mollitia, veniam libero, delectus quod porro amet iusto! Eum officia, necessitatibus laboriosam dicta nisi totam sit?</p>
+               <center>
+              	<a class="btn btn-primary" href="#" role="button">Read More</a>
+          	  </center>
+          </div>
+        </div>
+      </div>
+</div>
+
+    
---
+++ application/views/templates/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/style/style.css'); ?>">
+    <link href="https://fonts.googleapis.com/css?family=Ranga:700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+    <title><?= $title; ?></title>
+  </head>
+  <body>
+  	<nav class="navbar navbar-expand-lg p-3">
+      <div class="container">
+        <a class="navbar-brand" href="#"><span class="pdf">PDF</span><span class="reader">READER<span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#" title="Upload File Kamu"><i class="fas fa-cloud-upload-alt mr-2"></i>Upload</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" title="Lihat File PDF Kamu"><i class="fas fa-file-pdf mr-2"></i>Lihat File</a>
+        </li>
+      </ul>
+    </div>
+      </div>
+  </nav>
